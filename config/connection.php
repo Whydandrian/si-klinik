7233162fba306e01ba9912e71d450d3594bfefd5
whydandrian <whydandrian@gmail.com>
2021-07-22 14:57:13 +0700

    remove else condition at connection.php

--- config/connection.php
+++ config/connection.php
@@ -2,8 +2,6 @@
 $koneksi = mysqli_connect("localhost","root","","db_klinik_inova_medika");
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
-} else {
-	echo "Koneksi database berhasil";
 }
  
 ?>
