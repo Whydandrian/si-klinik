9a87cfe94d0959c91b53cb7e9d318722fe3ecd32
whydandrian <whydandrian@gmail.com>
2021-07-22 23:01:42 +0700

    modify query & add property harga to edit data obat

--- pages/obat_data/proses_edit.php
+++ pages/obat_data/proses_edit.php
@@ -4,10 +4,9 @@ include '../../config/connection.php';
 $id = $_POST['id'];
 $nama_obat = $_POST['nama_obat'];
 $jenis_obat = $_POST['jenis_obat'];
-// $harga = $_POST['harga'];
+$harga = $_POST['harga_obat'];
 
-// mysqli_query($koneksi,"UPDATE obat SET nama_obat='$nama_obat', jenis_obat='$jenis_obat', harga='$harga' WHERE id='$id'");
-mysqli_query($koneksi,"UPDATE obat SET nama_obat='$nama_obat', jenis_obat='$jenis_obat' WHERE id='$id'");
+mysqli_query($koneksi,"UPDATE obat SET nama_obat='$nama_obat', id_jenis_obat='$jenis_obat', harga_obat='$harga' WHERE id='$id'");
 header("location:../admin/data_obat.php?pesan=berhasil_update");
 
 ?>
