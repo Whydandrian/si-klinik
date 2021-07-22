521115e76dd2391454ecd7ce978641fa6474a602
whydandrian <whydandrian@gmail.com>
2021-07-22 11:46:28 +0700

    change database name

--- config/connection.php
+++ config/connection.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","klinik");
+$koneksi = mysqli_connect("localhost","root","","db_klinik_inova_medika");
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
