c10e47ae5f6f2baa7621c8bda779a9c58dc86e0f
whydandrian <whydandrian@gmail.com>
2021-07-22 22:21:53 +0700

    modify query for delete data user

--- pages/user_data/hapus.php
+++ pages/user_data/hapus.php
@@ -1,7 +1,7 @@
 <?php 
 include '../../config/connection.php';
 $id = $_GET['id'];
-mysqli_query($koneksi,"delete from user where id='$id'");
+mysqli_query($koneksi,"DELETE FROM user WHERE user_id='$id'");
 header("location:../admin/data_user.php?pesan=berhasil_hapus");
 
 ?>
