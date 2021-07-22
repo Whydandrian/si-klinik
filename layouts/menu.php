ac2e577666affd8f085a943f1a1b0e58d82ae69d
whydandrian <whydandrian@gmail.com>
2021-07-22 15:09:50 +0700

    modify username to nama_pegawai on menu

--- layouts/menu.php
+++ layouts/menu.php
@@ -68,7 +68,15 @@
       </ul>
       <li class="nav-item dropdown d-flex">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <?php echo $_SESSION['username']; ?>
+        <?php
+        $username = $_SESSION['username'];
+          $result = mysqli_query($koneksi, "SELECT `user`.*, `pegawai`.`nama_pegawai`, `pegawai`.`jenis_kelamin`, `pegawai`.`foto` FROM `pegawai` INNER JOIN `user` ON `user`.`kode_pegawai` = `pegawai`.`kode_pegawai` WHERE `user`.`username`='$username'");
+          while ($user_data = mysqli_fetch_array($result)) {
+            $nama_pegawai = $user_data['nama_pegawai'];
+            $foto = $user_data['foto'];
+          }
+          ?>
+        <?= $nama_pegawai;?>
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <li><a class="dropdown-item" href="../../logout.php">Logout</a></li>
